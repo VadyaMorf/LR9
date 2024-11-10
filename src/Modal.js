1d2932abab2a62a605d3eb5418be0215712035fa
VadyaMorf <152396090+VadyaMorf@users.noreply.github.com>
2024-11-11 01:11:56 +0200

    first commit

---
+++ src/Modal.js
@@ -0,0 +1,20 @@
+import React, { useCallback, useContext } from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { AppContext } from './context'
+const Modal = () => {
+
+  const {closeModal, isModalOpen} = useContext(AppContext);
+
+  return (
+    <div className={`modal-overlay ${isModalOpen && "show-modal"}`}>
+      <div className='modal-container'>
+        <h3>modal content</h3>
+        <button className='close-modal-btn' onClick={closeModal}>
+          <FaTimes/>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
